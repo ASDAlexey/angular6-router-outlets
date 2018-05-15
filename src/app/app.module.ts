ad30df76daa0080fd87fd786b0834e67d995a7f2
Popov <alexey.popov@arcadia.spb.ru>
2018-05-15 14:39:23 +0300

    make router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SecondComponent } from './second/second.component';
+import { FirstchildComponent } from './firstchild/firstchild.component';
+import { SecondchildComponent } from './secondchild/secondchild.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterializeModule } from 'angular2-materialize';
+import { RoutingModule } from './router.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SecondComponent,
+    FirstchildComponent,
+    SecondchildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterializeModule,
+    RoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
